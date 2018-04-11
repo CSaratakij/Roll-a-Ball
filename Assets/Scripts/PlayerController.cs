1ba8771e5f4380f5e4866a7318e7b0c0eb7c3656
Chatchai Saratakij <CSaratakij@gmail.com>
2018-04-11 07:57:50 +0100

    Make ball rolling.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace RollingBall
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        float moveForce;
+
+
+        Rigidbody rigid;
+        Vector3 inputVector;
+
+
+        void Awake()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            _InputHandler();
+        }
+
+        void FixedUpdate()
+        {
+            rigid.AddForce((inputVector * moveForce) * Time.deltaTime, ForceMode.Force);
+        }
+
+        void _InputHandler()
+        {
+            inputVector.x = Input.GetAxisRaw("Horizontal");
+            inputVector.z = Input.GetAxisRaw("Vertical");
+
+            if (inputVector.magnitude > 1)
+            {
+                inputVector = inputVector.normalized;
+            }
+        }
+    }
+}
